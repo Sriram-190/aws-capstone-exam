10a7965024dfee1508e66d8b715b7d8e68f8e997
Sriram-190 <125588647+Sriram-190@users.noreply.github.com>
2026-02-06 08:07:26 +0000

    Update index.php

--- webapp/index.php
+++ webapp/index.php
@@ -1,5 +1,17 @@
 <?php
 $ip = $_SERVER['SERVER_ADDR'];
 ?>
-<h1>Welcome to StreamLine - v2(new) [New Feature]</h1>
-<p>Server IP: <?php echo $ip; ?></p>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>StreamLine - v2(new) color</title>
+    <!-- Link to external CSS file -->
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Welcome to StreamLine - v2(new) color [New Feature]</h1>
+    <p>Server IP: <?php echo $ip; ?></p>
+</body>
+</html>
